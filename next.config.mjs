1660cd54c40e512ae454a0f5eb4060b7c718d36f
anujv99 <anujv@posteo.net>
2024-08-19 15:26:50 +0530

    enable wasm for webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
+  trailingSlash: true,
   output: "export",
-  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.experiments = { asyncWebAssembly: true, layers: true };
+    return config;
+  },
 };
 
 export default nextConfig;
