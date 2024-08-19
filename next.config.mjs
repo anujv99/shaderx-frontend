f6bbd9b1f1f7d3479280a2169ce5a838d2c0e9bc
anujv99 <anujv@posteo.net>
2024-08-19 15:40:05 +0530

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "",
-  trailingSlash: true,
-  output: "export",
   webpack: (config, options) => {
     config.experiments = { asyncWebAssembly: true, layers: true };
     return config;
